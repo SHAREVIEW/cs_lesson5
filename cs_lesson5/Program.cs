3e24685bb5d7033df2aebaa765b3b221d655cf6d
david <2657070472@qq.com>
2018-11-01 13:43:36 +0800

    变量类型转换溢出检查

--- cs_lesson5/Program.cs
+++ cs_lesson5/Program.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cs_lesson5
+namespace cs_lesson5  //溢出检查
 {
     class Program
     {
         static void Main(string[] args)
         {
+            byte destinationVar;
+            short sourceVar = 281;
+            destinationVar = checked((byte)sourceVar);            //checked,unchecked 表达式的溢出检查上下文
+            Console.WriteLine("sourceVar val:{0}",sourceVar);
+            Console.WriteLine("destinationVar val:{0}",destinationVar);
+            Console.ReadKey();
+
+            /*
+              281 = 100011001
+              25  = 000011001
+              255 = 011111111 
+           */
+            
+
         }
     }
 }
